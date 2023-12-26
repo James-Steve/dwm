c54e72669ec4f35e9ced96795c420ab0e9d671d3
James-Steve <hamsterofgold@gmail.com>
2023-12-26 14:07:49 +0200

    dwm COLOURED emojis working

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" , "MesloLGM Nerd Font Mono"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {  "MesloLGM Nerd Font Mono:size=10", "Noto Color Emoji:pixelsize=8:antialias=true:autohint=true"};
+static const char dmenufont[]       = "MesloLGM Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -59,8 +59,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "dmenu_run",  NULL};
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "dmenu_run",  NULL};
 static const char *termcmd[]  = { "st", NULL };
 static const char * slockcmd[] = {"slock", NULL};
 
