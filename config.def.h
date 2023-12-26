cc9e3b001efa8ee1b4d9e22c4ec29006fe84a6ea
Jasper Shovelton <Beanie.github@proton.me>
2023-12-26 12:57:35 +0000

    Add a `borderpx` value to `rules` in `config.def.h`.

--- config.def.h
+++ config.def.h
@@ -35,23 +35,19 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "ipscan",   NULL,    NULL,           0,         0,          0,           -1,       -1 },
-	{ "okular",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
-	{ "okular_sidebar",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
-	{ "okular_sidebar/Sidebar",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
-	{ "Okular",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
-	{ NULL ,"okular",      NULL,           0,         0,          0,           -1,        -1 },
-	{ "xclock",   NULL,    NULL,           0,         0,          0,           -1,       -1 },
-	{ "nautilus", NULL,    NULL,           0,         0,          0,           -1,        -1 },
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           -1,        -1 },
-	{ "gnome-calendar", NULL, NULL,        0,         0,          0,           -1,       -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "bash",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "st-256color", NULL, NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor   border width */
+	{ "ipscan",   NULL,    NULL,           0,         0,          0,           -1,       -1,        -1},
+	{ "xclock",   NULL,    NULL,           0,         0,          0,           -1,       -1,        -1},
+	{ "nautilus", NULL,    NULL,           0,         0,          0,           -1,       -1,        -1},
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           -1,       -1,        -1 },
+	{ "gnome-calendar", NULL, NULL,        0,         0,          0,           -1,       -1,        -1},
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1,        0},
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1,        -1},
+	{ "bash",      NULL,     NULL,           0,       0,          1,           0,        -1,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1,        -1 },
+	{ "st-256color", NULL, NULL,           0,         0,          1,           0,        -1,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1,        -1 }, /* xev */
+	/* class      instance    title       tags mask     isfloating   monitor   border width */
 };
 
 /* layout(s) */
--- dwm.c
+++ dwm.c
@@ -153,6 +153,7 @@ typedef struct {
 	int isterminal;
 	int noswallow;
 	int monitor;
+	int bw;
 } Rule;
 
 /* function declarations */
@@ -327,6 +328,7 @@ applyrules(Client *c)
 	/* rule matching */
 	c->isfloating = 0;
 	c->tags = 0;
+	c->bw = borderpx;
 	XGetClassHint(dpy, c->win, &ch);
 	class    = ch.res_class ? ch.res_class : broken;
 	instance = ch.res_name  ? ch.res_name  : broken;
@@ -341,6 +343,8 @@ applyrules(Client *c)
 			c->noswallow  = r->noswallow;
 			c->isfloating = r->isfloating;
 			c->tags |= r->tags;
+			if (r->bw != -1)
+				c->bw = r->bw;
 			for (m = mons; m && m->num != r->monitor; m = m->next);
 			if (m)
 				c->mon = m;
@@ -1211,7 +1215,6 @@ manage(Window w, XWindowAttributes *wa)
 		c->y = c->mon->wy + c->mon->wh - HEIGHT(c);
 	c->x = MAX(c->x, c->mon->wx);
 	c->y = MAX(c->y, c->mon->wy);
-	c->bw = borderpx;
 
 	wc.border_width = c->bw;
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
