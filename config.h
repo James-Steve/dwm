e88459ed9135dfa86f0a47319d89a81dc316914f
James-Steve <hamsterofgold@gmail.com>
2023-12-26 14:11:31 +0200

    dwm increased emoji font applied

--- config.h
+++ config.h
@@ -5,7 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {  "MesloLGM Nerd Font Mono:size=10", "Noto Color Emoji:pixelsize=8:antialias=true:autohint=true"};
+//                                          text font                           emoji font
+static const char *fonts[]          = {  "MesloLGM Nerd Font Mono:size=10", "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"};
 static const char dmenufont[]       = "MesloLGM Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
