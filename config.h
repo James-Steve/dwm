dc084617e380758ef81c6a6b35a2441ae7feacce
James-Steve <hamsterofgold@gmail.com>
2023-12-26 14:35:39 +0200

    dwm increased font size

--- config.h
+++ config.h
@@ -6,8 +6,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //                                          text font                           emoji font
-static const char *fonts[]          = {  "MesloLGM Nerd Font Mono:size=10", "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "MesloLGM Nerd Font Mono:size=10";
+static const char *fonts[]          = { "MesloLGM Nerd Font Mono:size=12", 
+                                          "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true"};
+static const char dmenufont[]       =   "MesloLGM Nerd Font Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
