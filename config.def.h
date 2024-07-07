a75084efcc103bd969b8dee03710d296c8ccf180
James-Steve <hamsterofgold@gmail.com>
2024-07-07 12:06:55 +0200

    dwm added more applications' terminal spawner to be swallowed

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //                                          text font                           emoji font
@@ -31,9 +31,21 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "ipscan",   NULL,    NULL,           0,         0,          0,           -1,       -1 },
+	{ "okular",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "okular_sidebar",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "okular_sidebar/Sidebar",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "Okular",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ NULL ,"okular",      NULL,           0,         0,          0,           -1,        -1 },
+	{ "xclock",   NULL,    NULL,           0,         0,          0,           -1,       -1 },
+	{ "nautilus", NULL,    NULL,           0,         0,          0,           -1,        -1 },
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           -1,        -1 },
+	{ "gnome-calendar", NULL, NULL,        0,         0,          0,           -1,       -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "bash",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st-256color", NULL, NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //                                          text font                           emoji font
@@ -29,9 +30,23 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "ipscan",   NULL,    NULL,           0,         0,          0,           -1,       -1 },
+	{ "okular",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "okular_sidebar",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "okular_sidebar/Sidebar",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ "Okular",  NULL,     NULL,           0,         0,          0,           -1,        -1 },
+	{ NULL ,"okular",      NULL,           0,         0,          0,           -1,        -1 },
+	{ "xclock",   NULL,    NULL,           0,         0,          0,           -1,       -1 },
+	{ "nautilus", NULL,    NULL,           0,         0,          0,           -1,        -1 },
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           -1,        -1 },
+	{ "gnome-calendar", NULL, NULL,        0,         0,          0,           -1,       -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "bash",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st-256color", NULL, NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
