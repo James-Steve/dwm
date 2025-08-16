6d3d14d34817315fc5e4082570c77c043a113f4e
James-Steve <hamsterofgold@gmail.com>
2025-08-16 22:44:08 +0200

    Bar is now dwmbar

--- bar.sh
+++ bar.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Terminate already running bar instances
-killall -q polybar
+dwmbar 2>&1 | tee -a /tmp/dwmbar.log & disown
+#killall -q dwmblocks
+#dwmblocks 2>&1 | tee-a /tmp/dwmblocks.log & disown
+#killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Polybar launched..."
+#polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+#3echo "Polybar launched..."
 
